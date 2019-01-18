7fa3f23db2b8fe4bb90c169568456da38caef228
Peejay Garcia <peejgarcia@gmail.com>
2019-01-17 19:10:40 -0600

    added some validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+import cgi
 
 app = Flask(__name__)
 
@@ -32,31 +33,61 @@ add_form = """
 """
 
 # a form for crossing off watched movies
+
+## TODO: We want to dynamically build the options using the current watchlist.
+## Use an accumulator method to accumulate the options.
+## options = "" and use a for loop to get each movie.
+##
+
+## TODO: Write a function that gets the current users watchlist.
+## Right now, we don't have a databse, so we can pretten to get
+## the watchlist by just returning a hardcoded list of movie titles.
+
+def get_current_watchlist():
+    return['Space Balls', 'The Goonies', 'The Sandlot', 'Begin Again', 'Once']
+
+
+options = ""
+for movie in get_current_watchlist():
+    options += '<option value="{0}">{0}</option>'.format(movie)
+    # 
+
 crossoff_form = """
     <form action="/crossoff" method="post">
         <label>
             I want to cross off
             <select name="crossed-off-movie"/>
-                <option value="Star Wars">Star Wars</option>
-                <option value="My Favorite Martian">My Favorite Martian</option>
-                <option value="The Avengers">The Avengers</option>
-                <option value="The Hitchhiker's Guide To The Galaxy">The Hitchhiker's Guide To The Galaxy</option>
+                {}
             </select>
             from my watchlist.
         </label>
         <input type="submit" value="Cross It Off"/>
     </form>
-"""
+""".format(options)
+
+
 
 
 @app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
     crossed_off_movie = request.form['crossed-off-movie']
-    crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
-    confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
-    content = page_header + "<p>" + confirmation + "</p>" + page_footer
 
-    return content
+    ## TODO: Validate that crossed_off_movie is in the current watchlist
+    ## If it is, do what we're doing already.
+    ## Else, send the meaningful error as a query parameter while
+    ## redirecting back to /. (/?error=SOMEERRORSTRINGHERE)
+    ## NOTE: Remember to escape the error message.
+    print("Move:", movie)
+    print("Crossed off movie:", crossed_off_movie)
+    if crossed_off_movie in get_current_watchlist():            
+        crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
+        confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
+        content = page_header + "<p>" + confirmation + "</p>" + page_footer
+
+        return content
+    error_url = "/?error={} isn't in your watchlist!".format(crossed_off_movie)
+    error_url_escaped = cgi.escape(error_url)
+    return redirect('/' + error_url)
 
 
 @app.route("/add", methods=['POST'])
@@ -74,11 +105,16 @@ def add_movie():
 @app.route("/")
 def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
+    error = request.args.get('error', False)
+    content = page_header + edit_header + add_form + crossoff_form + page_footer
+    if not error:
 
     # build the response string
-    content = page_header + edit_header + add_form + crossoff_form + page_footer
-
-    return content
+        
+        return content
+    
+    error_element =  "<p style='color:red;'>{}</p>".format(error)
+    return content + error_element
 
 
 app.run()
